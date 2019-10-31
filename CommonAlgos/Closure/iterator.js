05806900c1892c49645d9505a0284c1758818e86
Kajol Thapa <kajoolthapa@gmail.com>
2019-10-31 00:03:02 -0400

    simple iterator function

---
+++ CommonAlgos/Closure/iterator.js
@@ -0,0 +1,14 @@
+function returnIterator(arr) {
+    let i = 0;
+    return () => {
+      const element = arr[i];
+      i += 1;
+      return element;
+    }
+  }
+const array2 = ['a', 'b', 'c', 'd'];
+const myIterator = returnIterator(array2);
+console.log(myIterator()); // -> should log 'a'
+console.log(myIterator()); // -> should log 'b'
+console.log(myIterator()); // -> should log 'c'
+console.log(myIterator()); // -> should log 'd'
