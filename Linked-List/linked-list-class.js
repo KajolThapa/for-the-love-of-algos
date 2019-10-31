1a4e3f0a2d0dc469e5588a1417ecd4596244009a
Kajol Thapa <kajoolthapa@gmail.com>
2019-10-31 11:20:39 -0400

    added inserFirst, size, getFirst and getLast methods in linkedlist

---
+++ Linked-List/linked-list-class.js
@@ -0,0 +1,39 @@
+class Node {
+    constructor(data, next = null) {
+        this.data = data;
+        this.next = next;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+    inserFirst(data) {
+        this.head = new Node(data, this.head);
+    }
+    size() {
+        let counter = 0;
+        let node = this.head;
+        while (node) {
+            counter += 1;
+            node = node.next;
+        }
+        return counter;
+    }
+    getFirst() {
+        if (!this.head) {return;}
+        return this.head;
+        
+    }
+    getLast() {
+        if (!this.head) {return;}
+        let node = this.head;
+        while(node) { 
+            if (!node.next) {
+                return node;
+            }
+            node = node.next
+        }
+    }
+}
