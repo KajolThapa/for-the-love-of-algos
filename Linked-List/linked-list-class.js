e86b4f717b206251c2c9f59c1f220a346769d4d7
Kajol Thapa <kajoolthapa@gmail.com>
2019-10-31 11:35:28 -0400

    removes the last node in linkedlist

--- Linked-List/linked-list-class.js
+++ Linked-List/linked-list-class.js
@@ -50,4 +50,15 @@ class LinkedList {
         // }
 
     }
+    removeLast() {
+        if (!this.head) {return ;}
+        if (!this.head.next) {this.head = null;}
+        let prev = this.head;
+        let node = this.head.next;
+        while(node.next) {
+           prev = node;
+           node = node.next
+        }
+        prev.next = null;
+    }
 }
