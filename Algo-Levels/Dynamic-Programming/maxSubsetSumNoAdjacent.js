e30936700a9338ed28237fc8895597bc08571711
Kajol Thapa <kajoolthapa@gmail.com>
2020-02-24 21:30:02 -0500

    optimized solution in space O(1)

--- Algo-Levels/Dynamic-Programming/maxSubsetSumNoAdjacent.js
+++ Algo-Levels/Dynamic-Programming/maxSubsetSumNoAdjacent.js
@@ -7,18 +7,37 @@
         Sample output 330 (75, 120, 135)
  */
 
- //Time O(n) | Space O(n)
+//Time O(n) | Space O(n)
+
+// function maxSubsetSumNoAdjacent(array) {
+//     if (array.length === 0) return 0;
+//     if (array.length === 1) return array[0];
+
+//     const maxSums = array.slice();
+//     maxSums[1] = Math.max(maxSums[0], maxSums[1]);
+
+//     for (let i = 2; i < array.length; i++) {
+//         maxSums[i] = Math.max(maxSums[i-1], maxSums[i-2] + array[i]);   
+//      }
+//      return maxSums[maxSums.length - 1];
+
+// }
+
+
+ //Time O(n) | Space O(1)
  function maxSubsetSumNoAdjacent(array) {
      if (array.length === 0) return 0;
      if (array.length === 1) return array[0];
 
-    const maxSums = array.slice();
-    maxSums[1] = Math.max(maxSums[0], maxSums[1]);
+     let second = array[0];
+     let first = Math.max(array[0], array[1]);
 
     for (let i = 2; i < array.length; i++) {
-        maxSums[i] = Math.max(maxSums[i-1], maxSums[i-2] + array[i]);
+        const current = Math.max(first, second + array[i]);
+        second = first;
+        first = current;
     }
-    return maxSums[maxSums.length - 1];
+    return first;
  }
 
  console.log(maxSubsetSumNoAdjacent([75, 105, 120, 75, 90, 135]));
