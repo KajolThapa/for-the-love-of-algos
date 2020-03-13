3d961ffc8b754d4735830d406ff0bcc9a38c686e
Kajol Thapa <kajoolthapa@gmail.com>
2020-03-13 16:43:46 -0400

    recurrsive solution

---
+++ Binary-Search-Tree/closet-value-BST.js
@@ -0,0 +1,46 @@
+/**
+ * You are given a BST data structure consisting of BST nodes. 
+ * Each BST node has an integer value stored in a property called "value". and two children nodes stored in properties called "left" and "right", respectively.
+ * A node is said to be a BST node if and only if it satisfies the BST property: its value is strictly greater than the values of every node to its left;
+ * its value is less than or equal to the values of every node to its right; and both of its children nodes are either BST nodes themselves or null values.
+ * You are also given a target integer value; write a function that finds the closet value to the target value contained in the BST. Assume that there will only be one closet value.
+ * 
+ * 
+ * Sample Input:
+ *            10          , 12
+ *         /      \
+ *        5        15
+ *      /  \      /   \
+ *     2    5   13     22
+ *    /           \
+ *   1             14
+ * 
+ * Sample output: 13
+ */
+    
+
+ 
+// Solution 1  // if recurrsively solved Space complexity is same as Time.
+//Average:  O(log(N)) Time   |  O(log(N)) Space
+//Worst:    O(N) Time    |    O(N) Space 
+ function findClosetValue(BST, target) {
+     return findClosetValueBSTHelper(BST, target, Infinity)
+ }
+
+ //helper
+ function findClosetValueBSTHelper(tree, target, closest) {
+     if (tree === null) {
+         return closest;
+     }
+     if  (Math.abs(target - closest) > Math.abs(target - tree.value)) {
+         closest = tree.value;
+     }
+     if (target < tree.value) {
+         return findClosetValueBSTHelper(tree.left, target, closet)
+     } else if (target > tree.value) {
+        return findClosetValueBSTHelper(tree.right, target, closet)
+    } else {
+        return closet;
+    }
+
+ }
