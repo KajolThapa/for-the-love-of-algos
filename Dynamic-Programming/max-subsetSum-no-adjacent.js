3492d57d191dbaf2d65a997b0d0ac12260b60a11
Kajol Thapa <kajol.thapa.dev@gmail.com>
2020-08-06 13:15:59 -0400

    soution 1 in O(n) time & space

---
+++ Dynamic-Programming/max-subsetSum-no-adjacent.js
@@ -0,0 +1,30 @@
+/**
+ * Max Subset Sum No Adjacent
+ * 
+ * Write a function that takes in an array of positive integers and 
+ * returns the maximum sum of non-adjacent elements in the array
+ * 
+ * If a sum can't be generated, the function should return 0.
+ * 
+ * Sample Input:  array = [75, 105, 120, 75, 90, 135]
+ * 
+ * Sample Output:  330 // 75 + 120 + 135
+ */
+
+ //SOLUTION 1
+ // O(n) time | O(n) space
+
+ function maxSubsetSumNoAdjacent(arr) {
+     if (arr.length === 0) return 0;
+     if (arr.length === 1) return arr[0];
+     const maxSums = [...arr];  // create a new array of same length as input array
+     maxSums[1] = Math.max(maxSums[0], maxSums[1])
+    //  console.log(maxSums)
+     for (let i = 2; i < arr.length; i += 1) {
+         maxSums[i] = Math.max(maxSums[i-1], maxSums[i-2] + arr[i])
+     }
+     return maxSums[maxSums.length - 1]
+ }
+
+ console.log(maxSubsetSumNoAdjacent([7, 10, 12, 7, 9, 14]))
+ console.log(maxSubsetSumNoAdjacent([75, 105, 120, 75, 90, 135]))
