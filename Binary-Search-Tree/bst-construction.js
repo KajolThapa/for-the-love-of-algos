0428f4bd48e046141b38c0ffa7fbb5ae09ac16ca
KajolThapa <kajol.thapa.dev@gmail.com>
2020-08-18 23:29:01 -0400

    solved iteratively with Space O(1) at average and at worst cases

--- Binary-Search-Tree/bst-construction.js
+++ Binary-Search-Tree/bst-construction.js
@@ -1,4 +1,4 @@
-//Using Recurrsion
+// Recursively
 // At Average time & space both will be O(log(n)) - as we eliminate half of the tree in each traversal
 //At worst, O(n) time & space
 
@@ -74,7 +74,7 @@ class BST {
                 }
             } else if (parent.left === this) {       // case 3.3  where node only has either one childNode, or no childrenNodes 
                 parent.left = this.left !== null ? this.left : this.right;            
-            } else if (parent.right === this) { //case 3.4 when currentNode is rightNode of parentNode
+            } else if (parent.right === this) {      //case 3.4 when currentNode is rightNode of parentNode
                 parent.right = this.left !== null ? this.left : this.right;
             }
         }
@@ -89,4 +89,100 @@ class BST {
         }
 
     }
+}
+
+// Iteratively 
+// At Average Space O(1) | Time O(log(n))
+//At Worst Space O(1)    | Time O(n)
+
+class BinarySearchTree {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+
+    insert(value) {
+        let currentNode = this;
+        while (true) {
+            if (value < currentNode.value) {
+                if (currentNode.left === null) {
+                    currentNode.left = new BinarySearchTree(value);
+                    break;
+                } else {
+                    currentNode = currentNode.left;     
+                }
+            } else {
+                if (currentNode.right === null) {
+                    currentNode.right = new BinarySearchTree(value);
+                    break;
+                } else {
+                    currentNode = currentNode.right;
+                }
+            }
+        }
+        return this;
+    }
+
+    contains(value) {
+        let currentNode = this;
+        while (currentNode !== null) {
+            if (value < currentNode.value) {
+               currentNode = currentNode.left;
+            } else if (value > currentNode.value) {
+                currentNode = currentNode.right;
+            } else {
+                return true;
+            }
+        }
+       return false;
+    }
+
+    remove(value, parentNode = null) {
+        let currentNode = this;
+        while (currentNode !== null) {
+            if (value < currentNode.value) {
+                parentNode = currentNode;
+                currentNode = currentNode.left;
+            } else if (value > currentNode.value) {
+                parentNode = currentNode;
+                currentNode = currentNode.right;
+            } else { 
+                if (currentNode.left !== null && currentNode.right !== null) {
+                    currentNode.value = currentNode.right.getMinValue();
+                    currentNode.right.remove(currentNode.value, currentNode);
+                } else if (parentNode === null) {
+                    if (currentNode.left !== null) {
+                        currentNode.value = currentNode.left.value;
+                        currentNode.right = currentNode.left.right;
+                        currentNode.left = currentNode.left.left;
+
+                    } else if (currentNode.right !== null) {
+                        currentNode.value = currentNode.right.value;
+                        currentNode.left = currentNode.right.left;
+                        currentNode.right = currentNode.right.right;
+                    } else { 
+                        // no parentNode, no childrenNodes
+                        // single node - then do nothing
+                    }
+                } else if (parentNode.left === currentNode) {
+                    parentNode.left = currentNode.left !== null ? currentNode.left : currentNode.right;
+                } else if (parentNode.right === currentNode) {
+                    parentNode.right = currentNode.left !== null ? currentNodeleft : currentNode.right;
+                }
+                break;
+            }
+
+        }
+        return this;
+    }
+
+    getMinValue() {
+        let currentNode = this;
+        while (currentNode.rightleft !== null) {
+            currentNode = currentNode.left;
+        }
+        return currentNode.value;
+    }
+
 }
