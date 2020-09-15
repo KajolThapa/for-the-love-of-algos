da420c289e688e330f92af042b8ccc3a0fb008ee
KajolThapa <kajol.thapa.dev@gmail.com>
2020-09-14 23:41:40 -0400

    merge two sorted linked lists recursively

--- Linked-List/merge-linked-list.js
+++ Linked-List/merge-linked-list.js
@@ -61,3 +61,27 @@
  nine.next = ten;
 
 //  console.log(mergeLinkedLists(two, one));
+
+//using recursion
+// Time O(n + m)  |  Space(O(n + m))  where n, m are length of first & second linked List respectively
+function merge(headOne, headTwo) {
+    recursiveMerge(headOne, headTwo, null);
+    return headOne.value < headTwo.value ?  headOne : headTwo;
+}
+
+function recursiveMerge(p1, p2, p1Prev) {
+    if (p1 === null) {
+        p1Prev.next = p2;
+        return;
+    }
+    if (p2 === null) return;
+    
+    if (p1.value < p2.value) {
+        recursiveMerge(p1.next, p2, p1);
+    } else {
+        if (p1Prev !== null) p1Prev.next = p2;
+        const newP2 = p2.next;
+        p2.next = p1;
+        recursiveMerge(p1, newP2, p2)
+    }
+}
